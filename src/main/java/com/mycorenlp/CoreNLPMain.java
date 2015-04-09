59fcc4f486f077250a4cb659b59d4b44b372141c
Terry <jie.shi@hp.com>
2015-04-09 17:49:34 +0800

    1.add sentiment annotator 2.change stanford NLP ver to 1.5.1

--- src/main/java/com/mycorenlp/CoreNLPMain.java
+++ src/main/java/com/mycorenlp/CoreNLPMain.java
@@ -12,10 +12,12 @@ import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.SentimentAnnotator;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
@@ -36,26 +38,32 @@ public class CoreNLPMain {
     }
     
     public static void process() {
-     // creates a StanfordCoreNLP object, with POS tagging, lemmatization,
-        // NER, parsing, and coreference resolution
+        // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
         Properties props = new Properties();
-        props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+        props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref, sentiment");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
         // read some text in the text variable
-        String text = "Stanford University is located in California. It is a great university. Meg is CEO of HP. Tomorrow is a nice day!"; // Add your text here!
+        String text = "Stanford University is located in California. It is a great university. Meg is the 2nd female CEO of HP! This is a great PC. This PC is not so good after all!"; // Add your text here!
 
-        // create an empty Annotation just with the given text
-        Annotation document = new Annotation(text);
-
-        // run all Annotators on this text
-        pipeline.annotate(document);
+        //  approach 1
+        Annotation  document = pipeline.process(text);
+        
+        // approach 2
+//        // create an empty Annotation just with the given text
+//        Annotation document = new Annotation(text);
+//
+//        // run all Annotators on this text
+//        pipeline.annotate(document);
 
         // these are all the sentences in this document
         // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
         List<CoreMap> sentences = document.get(SentencesAnnotation.class);
 
         for (CoreMap sentence : sentences) {
+            System.out.println("--------------------- Begin of one sentence--------------------------");
+            System.out.println(sentence.toString());
+            
             // traversing the words in the current sentence
             // a CoreLabel is a CoreMap with additional token-specific methods
             for (CoreLabel token : sentence.get(TokensAnnotation.class)) { // TODO 还有哪些class? 
@@ -67,13 +75,19 @@ public class CoreNLPMain {
                 String ne = token.get(NamedEntityTagAnnotation.class);
                 System.out.println("word = " + word + " pos = " + pos + " ne = " + ne);
             }
+//            // this is the parse tree of the current sentence
+//            Tree treeAnnotation = sentence.get(TreeAnnotation.class);
+//            System.out.println("TreeAnnotation = " + treeAnnotation);
+//            // this is the Stanford dependency graph of the current sentence
+//            SemanticGraph dependencies = sentence
+//                    .get(CollapsedCCProcessedDependenciesAnnotation.class);
+            
+            // 情感分析
+            Tree treeSentiment = sentence
+                    .get(SentimentCoreAnnotations.AnnotatedTree.class);
+            int sentiment = RNNCoreAnnotations.getPredictedClass(treeSentiment);
+            System.out.println(printSentiment(sentiment));
             System.out.println("--------------------- End of one sentence--------------------------");
-            // this is the parse tree of the current sentence
-            Tree tree = sentence.get(TreeAnnotation.class);
-            System.out.println("TreeAnnotation = " + tree);
-            // this is the Stanford dependency graph of the current sentence
-            SemanticGraph dependencies = sentence
-                    .get(CollapsedCCProcessedDependenciesAnnotation.class);
         }
 
         // This is the coreference link graph
@@ -83,5 +97,42 @@ public class CoreNLPMain {
         // coreference 表示指代关系
         Map<Integer, CorefChain> graph = document.get(CorefChainAnnotation.class);
         System.out.println(graph);
+        
+    }
+    
+    
+    public static String getStrforSentiment(int mainSentiment) {
+        String sentiment = "";
+        switch (mainSentiment) {
+        case 0:
+            sentiment = "Very Happy";
+            break;
+        case 1:
+            sentiment = "Happy";
+            break;
+        case 2:
+            sentiment = "Neutral";
+            break;
+        case 3:
+            sentiment = "Sad";
+            break;
+        case 4:
+            sentiment = "Very Sad";
+            break;
+        }
+        return sentiment;
+    }
+    
+    public static String printSentiment(int id){
+        String sentiment = "";
+        switch(id){
+            case 0:sentiment = "Very Negative";break;
+            case 1:sentiment = "Negative";break;
+            case 2:sentiment = "Neutral";break;
+            case 3:sentiment = "Positive";break;
+            case 4:sentiment = "Very Positive";break;
+            default:break;
+        }
+        return sentiment;
     }
 }
